8bb0dbefa39532c6fa7766facdb1839c8538df05
sonanijaydip <sonanijaydip4@gmmail.com>
2024-05-28 19:22:48 +0530

    crud-operator

--- src/EmployeeData.js
+++ src/EmployeeData.js
@@ -9,19 +9,27 @@ export const EmployeeData = [
         id: 2,
         FirstName: 'kane',
         LastName: 'Williamson',
-        Age: 37
+        Age: 37,
+   
     },
     {
         id: 3,
-        FirstName: 'sanju',
-        LastName: 'samson',
-        Age: 28
+        FirstName: 'kne',
+        LastName: 'Wlliamson',
+        Age: 47,
+   
     },
-    {
-        id: 4,
-        FirstName: 'hello',
-        LastName: 'world',
-        Age: 26
-    },
+    // {
+    //     id: 3,
+    //     FirstName: 'sanju',
+    //     LastName: 'samson',
+    //     Age: 28
+    // },
+    // {
+    //     id: 4,
+    //     FirstName: 'hello',
+    //     LastName: 'world',
+    //     Age: 26
+    // },
 
 ]
